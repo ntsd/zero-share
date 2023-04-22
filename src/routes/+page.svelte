66f9617143071e5ab02cc6f579b9799e3e51130f
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-04-23 05:00:02 +0700

    refactor: move sender to component
    - move sender to component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,22 +3,14 @@
   import { addToastMessage } from '../stores/toastStore';
   import { defaultSendOptions } from '../configs';
   import Eye from '../components/Eye.svelte';
-  import { validateFileMetadata } from '../utils/validator';
-  import { Message, MetaData, ReceiveEvent, receiveEventToJSON } from '../proto/message';
-  import DragAndDrop from '../components/DragAndDrop.svelte';
-  import EventEmitter from 'eventemitter3';
+  import { Message } from '../proto/message';
   import Collapse from '../components/Collapse.svelte';
-  import SendingFileList from '../components/SendingFileList.svelte';
   import SenderOptions from '../components/SenderOptions.svelte';
-  import { FileStatus, type SendOptions, type SendingFile } from '../type';
-  import {
-    encryptAesGcm,
-    encryptAesKeyWithRsaPublicKey,
-    generateAesKey,
-    importRsaPublicKeyFromBase64
-  } from '../utils/crypto';
+  import { importRsaPublicKeyFromBase64 } from '../utils/crypto';
   import { sdpDecode, sdpEncode } from '../utils/sdpEncode';
   import Spinner from '../components/Spinner.svelte';
+  import type { SendOptions } from '../type';
+  import Sender from '../components/Sender.svelte';
 
   let sendOptions = defaultSendOptions;
   let isConnecting = false;
@@ -31,6 +23,8 @@
   let offerLink = '';
   let showOfferLink = false;
   let answerSDP = '';
+
+  let sender: Sender;
 
   async function connectPeerAndCreateDataCannel() {
     connection = new RTCPeerConnection({
@@ -49,11 +43,8 @@
     dataChannel.onmessage = (ev) => {
       const message = Message.decode(new Uint8Array(ev.data));
 
-      if (message.receiveEvent) {
-        const sendingFile = sendingFiles[message.id];
-        if (sendingFile && sendingFile.event) {
-          sendingFile.event.emit(receiveEventToJSON(message.receiveEvent));
-        }
+      if (message.receiveEvent !== undefined) {
+        sender.onReceiveEvent(message.id, message.receiveEvent);
       }
     };
     dataChannel.onerror = () => {
@@ -89,10 +80,6 @@
     await connection.setLocalDescription(offer);
   }
 
-  function toggleOfferLinkVisibility() {
-    showOfferLink = !showOfferLink;
-  }
-
   async function copyOfferLink() {
     await navigator.clipboard.writeText(offerLink);
     addToastMessage('Copied to clipboard', 'info');
@@ -110,170 +97,6 @@
       sdp: sdpDecode(sdp, false)
     };
     await connection.setRemoteDescription(remoteDesc);
-  }
-
-  async function onSend(key: string) {
-    const sendingFile = sendingFiles[key];
-    let offset = 0;
-
-    // reset value
-    sendingFiles[key].error = undefined;
-    sendingFiles[key].stop = false;
-
-    sendingFiles[key].event = new EventEmitter();
-
-    sendingFiles[key].event?.on(
-      receiveEventToJSON(ReceiveEvent.EVENT_RECEIVER_ACCEPT),
-      async () => {
-        sendingFiles[key].status = FileStatus.Processing;
-        sendingFiles[key].startTime = Date.now();
-        await sendNextChunk();
-      }
-    );
-    sendingFiles[key].event?.on(receiveEventToJSON(ReceiveEvent.EVENT_RECEIVED_CHUNK), async () => {
-      if (sendingFiles[key].stop) {
-        return;
-      }
-      if (sendingFiles[key].error || sendingFiles[key].status != FileStatus.Processing) {
-        sendingFiles[key].progress = 0;
-        sendingFiles[key].status = FileStatus.Pending;
-        sendingFiles[key].stop = false;
-        return;
-      }
-
-      if (offset < sendingFile.metaData.size) {
-        await sendNextChunk();
-        return;
-      }
-
-      sendingFiles[key].status = FileStatus.Success;
-      addToastMessage(`File ${sendingFile.metaData.name} sent successfully`, 'success');
-    });
-
-    sendingFiles[key].event?.on(receiveEventToJSON(ReceiveEvent.EVENT_VALIDATE_ERROR), () => {
-      addToastMessage('Receiver validate error', 'error');
-      sendingFiles[key].error = new Error('Receiver validate error');
-      sendingFiles[key].status = FileStatus.Pending;
-    });
-
-    sendingFiles[key].event?.on(receiveEventToJSON(ReceiveEvent.EVENT_RECEIVER_REJECT), () => {
-      addToastMessage('Receiver reject the file', 'error');
-      sendingFiles[key].error = new Error('Receiver reject the file');
-      sendingFiles[key].status = FileStatus.Pending;
-    });
-
-    async function sendBuffer(buffer: ArrayBuffer) {
-      if (sendOptions.isEncrypt) {
-        const aesKey = sendingFiles[key].aesKey;
-        if (aesKey) {
-          const encrypted = await encryptAesGcm(aesKey, buffer);
-          dataChannel.send(
-            Message.encode({
-              id: sendingFile.metaData.name,
-              chunk: encrypted
-            }).finish()
-          );
-          return;
-        }
-      }
-
-      dataChannel.send(
-        Message.encode({
-          id: sendingFile.metaData.name,
-          chunk: new Uint8Array(buffer)
-        }).finish()
-      );
-    }
-
-    async function sendNextChunk() {
-      const slice = sendingFile.file.slice(offset, offset + sendOptions.chunkSize);
-      const buffer = await slice.arrayBuffer();
-
-      await sendBuffer(buffer);
-
-      offset += buffer.byteLength;
-
-      // calculate progress
-      sendingFiles[key].progress = Math.round((offset / sendingFile.metaData.size) * 100);
-
-      // calculate bitrate
-      sendingFiles[key].bitrate = Math.round(
-        offset / ((Date.now() - sendingFiles[key].startTime) / 1000)
-      );
-    }
-
-    // send meta data
-    dataChannel.send(
-      Message.encode({
-        id: sendingFile.metaData.name,
-        metaData: sendingFile.metaData
-      }).finish()
-    );
-
-    sendingFiles[key].status = FileStatus.WaitingAccept;
-    // TODO: wait finish to send 1 by 1 file (success, error)
-  }
-
-  async function sendAllFiles() {
-    for (const key of Object.keys(sendingFiles)) {
-      if (sendingFiles[key].status != FileStatus.Pending || sendingFiles[key].error) {
-        continue;
-      }
-      await onSend(key);
-    }
-  }
-
-  async function onStop(key: string) {
-    sendingFiles[key].stop = true;
-  }
-
-  async function onContinue(key: string) {
-    sendingFiles[key].stop = false;
-    sendingFiles[key].event?.emit(receiveEventToJSON(ReceiveEvent.EVENT_RECEIVED_CHUNK));
-  }
-
-  let sendingFiles: { [key: string]: SendingFile } = {};
-
-  function onRemove(key: string) {
-    if (sendingFiles[key].status === FileStatus.Processing) {
-      sendingFiles[key].stop = true;
-    }
-    delete sendingFiles[key];
-    sendingFiles = sendingFiles; // do this to trigger update the map
-  }
-
-  function onFilesPick(files: FileList) {
-    Array.from(files).forEach(async (file) => {
-      let aesKey;
-      let aesEncrypted = new Uint8Array();
-      if (sendOptions.isEncrypt) {
-        aesKey = await generateAesKey();
-        aesEncrypted = await encryptAesKeyWithRsaPublicKey(rsaPub, aesKey);
-      }
-
-      const fileMetaData: MetaData = {
-        name: file.name,
-        size: file.size,
-        type: file.type,
-        key: aesEncrypted
-      };
-      const validateErr = validateFileMetadata(fileMetaData);
-      if (validateErr) {
-        addToastMessage(`${file.name} ${validateErr.message}`, 'error');
-      }
-
-      sendingFiles[file.name] = {
-        file: file,
-        metaData: fileMetaData,
-        progress: 0,
-        bitrate: 0,
-        stop: false,
-        error: validateErr,
-        startTime: 0,
-        status: FileStatus.Pending,
-        aesKey: aesKey
-      };
-    });
   }
 
   function onOptionsUpdate(options: SendOptions) {
@@ -306,7 +129,12 @@
         value={offerLink}
         readonly
       />
-      <button class="absolute top-2 right-2 p-2" on:click={toggleOfferLinkVisibility}>
+      <button
+        class="absolute top-2 right-2 p-2"
+        on:click={() => {
+          showOfferLink = !showOfferLink;
+        }}
+      >
         <Eye show={showOfferLink} />
       </button>
     </div>
@@ -320,13 +148,5 @@
 </Collapse>
 
 <Collapse title="3. Send Files" isOpen={isConnecting}>
-  <div class="grid gap-4">
-    <DragAndDrop {onFilesPick} />
-    {#if Object.keys(sendingFiles).length > 0}
-      <SendingFileList {sendingFiles} {onRemove} {onSend} {onStop} {onContinue} />
-      <button class="btn btn-primary mt-2" on:click={sendAllFiles}>Send all files</button>
-    {:else}
-      <p class="mt-4">No files selected</p>
-    {/if}
-  </div>
+  <Sender bind:this={sender} {dataChannel} {rsaPub} {sendOptions} />
 </Collapse>

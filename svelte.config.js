04a1d96feabfbfa46d94037cd49eadce2e9cd81d
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-08-03 13:37:59 +0700

    feat: add sveltekit html minifier

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import htmlMinifierAdapter from 'sveltekit-html-minifier';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: htmlMinifierAdapter(adapter()),
     paths: {},
     prerender: {}
   }

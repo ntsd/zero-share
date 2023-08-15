e8c7271a7da710251c0e114936b5a9819e18bd6f
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-08-15 07:12:16 +0700

    fix: convert navbar to astro

--- src/stores/darkModeStore.ts
+++ src/stores/darkModeStore.ts
@@ -1,37 +1,35 @@
-import { get, writable } from 'svelte/store';
+import { atom } from 'nanostores';
 
 const localStorageKey = 'darkMode';
+
+// get dark mode from local storage or system media
+let defaultDarkMode = false;
+const localDarkMode = localStorage.getItem(localStorageKey);
+switch (localDarkMode) {
+  case 'true':
+    defaultDarkMode = true;
+    break;
+  case 'false':
+    defaultDarkMode = false;
+    break;
+  case null:
+    defaultDarkMode = window?.matchMedia?.('(prefers-color-scheme:dark)')?.matches || false;
+}
+
+export const darkModeAtom = atom<boolean>(defaultDarkMode);
 
 function setTheme(darkMode: boolean) {
   document?.querySelector('html')?.setAttribute('data-theme', darkMode ? 'dark' : 'light');
 }
 
-const darkModeStore = writable<boolean>(
-  window?.matchMedia?.('(prefers-color-scheme:dark)')?.matches || false
-);
-
-// get dark mode from local storage
-const localDarkMode = localStorage.getItem(localStorageKey);
-switch (localDarkMode) {
-  case 'true':
-    setDarkMode(true);
-    break;
-  case 'false':
-    setDarkMode(false);
-}
-
 export function getDarkMode(): boolean {
-  return get(darkModeStore);
+  return darkModeAtom.get();
 }
 
 export function setDarkMode(darkMode: boolean): void {
   localStorage.setItem(localStorageKey, darkMode ? 'true' : 'false');
   setTheme(darkMode);
-  darkModeStore.update(() => {
-    return darkMode;
-  });
+  darkModeAtom.set(darkMode);
 }
-
-export default darkModeStore;
 
 setTheme(getDarkMode());
